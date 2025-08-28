cf0bfe84510a85ada735ab9b56d7812dd63c64de
deepalakshmi-07 <deepalakshmi2378@gmail.com>
2025-08-28 12:22:12 +0530

    changes in server page

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post("/clerk", express.json(), clerkWebhooks);
 app.use("/api/educator", express.json(), requireAuth(), educatorRouter);
 app.use("/api/course", express.json(), courseRouter);
 app.use("/api/user", express.json(), userRouter);
-app.post("/stripe", express.raw({ type: "application" }), stripeWebhooks);
+app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 //Port
 const PORT = process.env.PORT || 5000;
